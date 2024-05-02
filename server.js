34bf2f31ed9836cc0e6ddf7a960cdda074393842
yesrab <yesrab.ali111@gmail.com>
2024-05-02 19:44:32 +0530

    basic express server setup and database connection

--- server.js
+++ server.js
@@ -1,1 +1,59 @@
 //node server
+const express = require("express");
+require("dotenv").config();
+require("express-async-errors");
+const statusMonitor = require("express-status-monitor-plus");
+const cors = require("cors");
+//env
+const PORT = process.env.PORT || 8080;
+const DB_URI = process.env.DB;
+//env
+
+//init express app
+const app = express();
+
+const connectDB = require("./db/connect");
+
+//cors middleware
+app.use(cors());
+
+//express body json parsing middleware
+app.use(express.json());
+
+//express url parsing middleware
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  statusMonitor({
+    path: "/api/status",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello from the server",
+    status: "success",
+    currentTime: new Date().toISOString(),
+    path: req.path,
+    url: req.originalUrl,
+  });
+});
+
+//start server and connet to db
+const startServer = async () => {
+  try {
+    await connectDB(DB_URI);
+    console.log("DB connected");
+    app.listen(PORT, () => {
+      console.log(`Server Started at port ${PORT}`);
+      console.log("");
+      console.log("\x1b[36m%s\x1b[0m", `http://localhost:${PORT}/`);
+      console.log("^ click here");
+      console.log("");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
